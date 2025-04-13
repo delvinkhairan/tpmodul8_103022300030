28f83732bb485c372e4a9e3d48569c9da19fbd1a
delvinkhairan <delvinkhairan9558@gmail.com>
2025-04-13 19:03:33 +0700

    final commit program selesai

---
+++ CovidConfig.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+public class CovidConfig
+{
+    public string SatuanSuhu { get; set; }
+    public int BatasHariDeman { get; set; }
+    public string PesanDitolak { get; set; }
+    public string PesanDiterima { get; set; }
+
+    public CovidConfig()
+    {
+        // Nilai default kalo file config belum ada
+        SatuanSuhu = "celcius";
+        BatasHariDeman = 14;
+        PesanDitolak = "Anda tidak diperbolehkan masuk ke dalam gedung ini";
+        PesanDiterima = "Anda dipersilahkan untuk masuk ke dalam gedung ini";
+
+        // Load config dari file (jika ada)
+        LoadConfig();
+    }
+
+    private void LoadConfig()
+    {
+        if (File.Exists("covid_config.json"))
+        {
+            string json = File.ReadAllText("covid_config.json");
+            dynamic config = JsonConvert.DeserializeObject(json);
+
+            SatuanSuhu = config.satuan_suhu ?? SatuanSuhu;
+            BatasHariDeman = config.batas_hari_deman ?? BatasHariDeman;
+            PesanDitolak = config.pesan_ditolak ?? PesanDitolak;
+            PesanDiterima = config.pesan_diterima ?? PesanDiterima;
+        }
+    }
+
+    public void UbahSatuan()
+    {
+        SatuanSuhu = (SatuanSuhu == "celcius") ? "fahrenheit" : "celcius";
+        Console.WriteLine($"Satuan suhu diganti ke: {SatuanSuhu}");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CovidConfig config = new CovidConfig();
+
+        // Input suhu
+        Console.WriteLine($"Berapa suhu badan anda saat ini? Dalam nilai {config.SatuanSuhu}: ");
+        double suhu = Convert.ToDouble(Console.ReadLine());
+
+        // Input hari demam
+        Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam? ");
+        int hariDeman = Convert.ToInt32(Console.ReadLine());
+
+        // Validasi
+        bool suhuValid = (config.SatuanSuhu == "celcius")
+            ? (suhu >= 36.5 && suhu <= 37.5)
+            : (suhu >= 97.7 && suhu <= 99.5);
+        bool hariValid = hariDeman < config.BatasHariDeman;
+
+        // Output pesan
+        Console.WriteLine(hariValid && suhuValid ? config.PesanDiterima : config.PesanDitolak);
+
+        // Bonus: Ubah satuan
+        Console.WriteLine("\nTekan 'U' untuk ubah satuan suhu...");
+        if (Console.ReadKey().Key == ConsoleKey.U)
+        {
+            config.UbahSatuan();
+        }
+    }
+}
